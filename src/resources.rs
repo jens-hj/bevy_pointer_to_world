b385d01ca25d9654cd241c725b31eeb791ef5735
jens-hj <jens.jens@live.dk>
2025-02-02 23:19:44 +0100

    feat(resource): impl Display for PointerWorldPosition Resource

--- src/resources.rs
+++ src/resources.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
 
 /// Bevy [`Resource`] to store the current pointer position in world coordinates
-#[derive(Resource, Default)]
+#[derive(Resource, Default, Debug)]
 pub struct PointerWorldPosition(pub Vec2);
+
+impl std::fmt::Display for PointerWorldPosition {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "PointerWorldPosition({:.2}, {:.2})", self.0.x, self.0.y)
+    }
+}
